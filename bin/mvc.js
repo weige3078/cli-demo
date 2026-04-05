642bd34d6dee4495aead20825bd15b70c33a4595
1688wdw <1985734033@qq.com>
2026-04-05 11:48:24 +0800

    添加脚手架的update命令

--- bin/mvc.js
+++ bin/mvc.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const path = require('path')
+const chalk = require('chalk')
 const program = require('commander')
 const create = require('../lib/create')
+const update = require('../lib/update')
 const { version } = require(path.join(__dirname, '../package.json'))
 
 program
@@ -11,6 +13,19 @@ program
 .description('create a new project')
 .action((name, cmd) => { 
     create(name, { skipInstall: !!cmd.skipInstall })
+})
+
+program
+.command('update')
+.description('update global mvc CLI')
+.option('--registry <url>', 'npm registry url')
+.action(async (opts) => {
+    try {
+        await update(opts)
+    } catch (e) {
+        console.error(chalk.red(e.message || e))
+        process.exit(1)
+    }
 })
 
 program.parse()
